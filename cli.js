5f2c202f7a9a903622c5da008f860a4bf89f9580
slior <lior.schejter@gmail.com>
2016-05-27 00:00:53 +0300

    Adding get properties command

--- cli.js
+++ cli.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const vorpal = require('vorpal')();
 
 //Setting up configuration types
@@ -5,7 +7,8 @@ const ConfigRegistry = require("./app/core/config/ConfigTypeRegistry.js")
 const dbg = console.log
 
 const configList = require('./app/core/configList.js')
-
+const ConfigResource = require("./app/core/configResource.js")
+const configFile = require("./app/core/configFile.js")
 
 vorpal
   .command('list', 'List available config files')
@@ -16,5 +19,23 @@ vorpal
   });
 
 vorpal
-  .delimiter('config$')
+  .command('props <file>','List properties of a given file')
+  .action(function(args,callback) {
+    var self = this
+    let CR = ConfigResource
+    let resource = CR.resolve(args.file)
+    if (resource.type == CR.FILE_RESOURCE)
+    {
+      let props = configFile.readFile(resource.fspath)
+      Object.keys(props)
+            .map(name => name + " = " + props[name])
+            .forEach(o => self.log(o))
+    }
+    else self.log("ERROR: not a file")
+
+    callback()
+  });
+
+vorpal
+  .delimiter('config>>')
   .show();
